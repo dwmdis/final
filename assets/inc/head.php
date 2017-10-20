8ccbe7e914112f8401fc93c27daaf6cece38f4b5
Dale Moore <dmoore092@gmail.com>
2017-10-20 13:53:47 -0400

    fixed nav functionality

---
+++ assets/inc/head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title><?php echo $title; ?></title>
+    <!-- Set device width to appropriate values, and name css files accordingly -->
+    <!--<link type="text/css" href="assets/css/main.css" rel="stylesheet"> -->
+    <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="assets/css/main.css" />
+    <link rel='stylesheet' media='screen and (min-width: 300px) and (max-width: 799px)' href='assets/css/medium.css' />
+    <script src="assets/scripts/mobileNav.js"></script>
+</head>
--- index.php
+++ index.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php 
-    //Sets variables to be used
+    //Sets variables
     $relpath= ""; $title="Vi|home"; $page="home"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
 ?>
-<?php include "<?php $relpath; ?>assets/inc/head.php" ?>
-<head><!-- This will be stripped out later to /inc-->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=700, initial-scale=1">
-    <title><?php echo $title; ?></title>
-    <!-- Set device width to appropriate values, and name css files accordingly -->
-    <!--<link type="text/css" href="assets/css/main.css" rel="stylesheet"> -->
-    <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="assets/css/main.css" />
-    <link rel='stylesheet' media='screen and (min-width: 601px) and (max-width: 799px)' href='assets/css/medium.css' />
-    <script src="assets/scripts/mobileNav.js"></script>
-</head>
+<?php include "assets/inc/head.php" ?>
+<!-- <head> went here -->
 <body>
-    <nav id="myMobilenav" class="mobilenav">
-        <a id="close-nav" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="http://serenity.ist.rit.edu/~iste240t04/240/final/index.php">Home</a>
-        <a href="#">Intermediate</a>
-        <a href="#">Advanced</a>
-        <a href="#">About</a>
-    </nav>
     <span id="click-here" onclick="openNav()">Click Here!</span>
+    <header>
+        <!-- <img src="http://lorempixel.com/400/200/" alt="logo"/> -->
+    </header>
+    <nav id="myMobilenav" class="mobilenav">
+        <ul>
+            <li><a id="close-nav" href="#" class="closebtn" onclick="closeNav()">&times;</a></li>
+            <li><a href="http://serenity.ist.rit.edu/~iste240t04/240/final/index.php">Home</a>
+            <li><a href="#">Intermediate</a></li>
+            <li><a href="#">Advanced</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+            
+    </nav>
   <main>
-    <div>HTML Boilerplate test</div>
+    <div>HTML Boilerplate test
+      "javascript:void(0)"
+      </div>
   </main>
 </body>
 
