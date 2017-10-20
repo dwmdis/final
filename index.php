d5b6dd39abe07134bb62c9920413d388e62111ad
Dale Moore <dmoore092@gmail.com>
2017-10-20 13:13:34 -0400

    removed all the styling and mobile functionality for non-mobile version of site

--- index.php
+++ index.php
@@ -10,20 +10,20 @@
     <meta name="viewport" content="width=700, initial-scale=1">
     <title><?php echo $title; ?></title>
     <!-- Set device width to appropriate values, and name css files accordingly -->
-    <link type="text/css" href="assets/css/main.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="assets/css/bleh.css" />
-    <link rel='stylesheet' media='screen and (min-width: 701px) and (max-width: 900px)' href='assets/css/blah.css' /> -->
+    <!--<link type="text/css" href="assets/css/main.css" rel="stylesheet"> -->
+    <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="assets/css/main.css" />
+    <link rel='stylesheet' media='screen and (min-width: 601px) and (max-width: 799px)' href='assets/css/medium.css' />
     <script src="assets/scripts/mobileNav.js"></script>
 </head>
 <body>
     <nav id="myMobilenav" class="mobilenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a id="close-nav" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="http://serenity.ist.rit.edu/~iste240t04/240/final/index.php">Home</a>
         <a href="#">Intermediate</a>
         <a href="#">Advanced</a>
         <a href="#">About</a>
     </nav>
-    <span onclick="openNav()">Click Here!</span>
+    <span id="click-here" onclick="openNav()">Click Here!</span>
   <main>
     <div>HTML Boilerplate test</div>
   </main>
