2dc5c5030d77ae2bcaa66ed3ee5bffb1e3e854c3
Dale Moore <dmoore092@gmail.com>
2017-10-20 12:01:58 -0400

    added the beginning of php code to <head> plus added php variables

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8" />
-  <title></title>
-  <link type="text/css" href="assets/css/main.css" rel="stylesheet">
+<?php 
+    //Sets variables to be used
+    $relpath= ""; $title="Vi|home"; $page="home"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
+?>
+<?php include "<?php $relpath; ?>assets/inc/head.php" ?>
+<head><!-- This will be stripped out later to /inc-->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=700, initial-scale=1">
+    <title><?php echo $title; ?></title>
+    <!-- Set device width to appropriate values, and name css files accordingly -->
+    <link type="text/css" href="assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="assets/css/bleh.css" />
+    <link rel='stylesheet' media='screen and (min-width: 701px) and (max-width: 900px)' href='assets/css/blah.css' />
 </head>
 <body>
   <main>
