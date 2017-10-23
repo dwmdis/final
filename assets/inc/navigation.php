24d6cbc60584b0041141a99f805ad83dd2fe60a6
Dale Moore <dmoore092@gmail.com>
2017-10-23 15:06:29 -0400

    moved navigation to php include statement and messed with web view nav some more

---
+++ assets/inc/navigation.php
@@ -0,0 +1,42 @@
+    <span id="click-here" onclick="openNav()">Click Here!</span>
+    <header>
+        <image id="logos">
+        <!-- <img id="big-logo" src="http://lorempixel.com/400/200/" alt="big-logo"/>
+        <img id="small-logo" src="http://lorempixel.com/100/50/" alt="small-logo" /> -->
+        </image>
+        <form id="search-box">
+            <input type="search" placeholder="Search this site..." />
+            <input type="submit" value="Search" class="btn-search"  />
+        </form>
+    </header>
+    <nav id="myMobilenav" class="mobilenav">
+            <a id="close-nav" href="#" class="closebtn" onclick="closeNav(); closeSubNav()">&times;</a>
+            <div class="sub-navlist">    
+                <a onclick="closeSubNav(); getId(this); openSubNav()" id="home-section" <?php if($page == 'home'){  echo 'class="active"';}?> href="#">Home</a>
+                <div class="float-container">    
+                    <a class="subnav1" href="#">Home Sub1</a>
+                    <a class="subnav1" href="#">Home Sub2</a>
+                    <a class="subnav1" href="#">Home Sub3</a>
+                </div>
+            </div><!-- end <div class="sub-navlist"> -->
+            <div class="sub-navlist">
+                <a onclick="closeSubNav(); getId(this); openSubNav()" id="intermediate-section" <?php if($page == 'intermediate'){  
+                echo 'class="active"';}?> href="#">Intermediate</a>
+                <div class="float-container">
+                    <a class="subnav2" href="intermediate.php">Intermediate Sub1</a>
+                    <a class="subnav2" href="#">Intermediate Sub2</a>
+                    <a class="subnav2" href="#">Intermediate Sub3</a>
+                </div>
+            </div><!-- END <div class="sub-navlist> -->
+            <div class="sub-navlist">
+                <a onclick="closeSubNav(); getId(this); openSubNav()" id="advanced-section" <?php if($page == 'advanced'){ 
+                echo 'class="active"';}?> href="#">Advanced</a>
+                <div class="float-container">
+                    <a class="subnav3" href="#">Advanced Sub1</a>
+                    <a class="subnav3" href="#">Advanced Sub2</a>
+                    <a class="subnav3" href="#">Advanced Sub3</a>
+                </div>
+            </div><!-- END <div class="sub-navlist> -->
+            <a <?php if($page == 'about'){ 
+                echo 'class="active"';}?> href="#">About</a>
+    </nav>
--- intermediate.php
+++ intermediate.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <?php 
     //Sets variables
-    $relpath= ""; $title="Vi|home"; $page="home"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
+    $relpath= ""; $title="Vi|Intermediate"; $page="intermediate"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
 ?>
 <?php include "assets/inc/head.php" ?>
 <!-- <head> went here -->
 <body>
 <?php include "assets/inc/navigation.php" ?>
   <main>
-    <div>HTML Boilerplate test
+    <div>Intermediate Page
         <p>page = "<?php echo $page ?>"</p>
       </div>
   </main>
