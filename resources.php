92434d5f4b2c067e0613179f1433ef97880cdd0b
Dale Moore <dmoore092@gmail.com>
2017-11-08 14:30:38 -0500

    pushing correct php files

---
+++ resources.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+    //Sets variables
+    $relpath= ""; $title="Vi|Resources"; $page="resources"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
+?>
+<?php include "assets/inc/head.php" ?>
+<!-- <head> went here -->
+<body>
+<?php include "assets/inc/navigation.php" ?>
+  <main>
+    <div>Home page
+        <p>page = "<?php echo $page ?>"</p>
+        <p>Resources content here.</p>
+      </div>
+  </main>
+</body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+    //Sets variables
+    $relpath= ""; $title="Vi|Test of Skill"; $page="test"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
+?>
+<?php include "assets/inc/head.php" ?>
+<!-- <head> went here -->
+<body>
+<?php include "assets/inc/navigation.php" ?>
+  <main>
+    <div>Home page
+        <p>page = "<?php echo $page ?>"</p>
+        <p>Test of skill here</p>
+      </div>
+  </main>
+</body>
+
+</html>
