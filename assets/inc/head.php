ccf7157279c39af7153e4813d47c2d41d2d1eb37
Dale Moore <dmoore092@gmail.com>
2017-11-08 14:29:41 -0500

    fixed main.css media query for web view, created all of the remaining content pages, worked on navigation links(unfinished)

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -2,9 +2,6 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <title><?php echo $title; ?></title>
-    <!-- Set device width to appropriate values, and name css files accordingly -->
-    <!--<link type="text/css" href="assets/css/main.css" rel="stylesheet"> -->
-    <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="assets/css/main.css" />
-    <link rel='stylesheet' media='screen and (min-width: 300px) and (max-width: 799px)' href='assets/css/medium.css' />
-    <script src="assets/scripts/mobileNav.js"></script>
+    <link type="text/css" href="<?= $relpath ?>assets/css/main.css" rel="stylesheet">
+    <script src="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/scripts/mobileNav.js"></script>
 </head>
--- assets/inc/navigation.php
+++ assets/inc/navigation.php
@@ -1,3 +1,6 @@
+    <?php 
+        $linkpath = "http://serenity.ist.rit.edu/~iste240t04/240/final/"
+    ?>
     <span id="click-here" onclick="openNav()">Click Here!</span>
     <header>
         <image id="logos">
@@ -13,32 +16,39 @@
             <a id="close-nav" href="#" class="closebtn" onclick="closeNav(); closeSubNav()">&times;</a>
             <div class="sub-navlist">    
                 <a onclick="closeSubNav(); getId(this); openSubNav()" id="home-section" <?php if($page == 'home'){  echo 'class="active"';}?> href="#">Home</a>
-                <div class="float-container">    
-                    <a class="subnav1" href="index.php">Home Sub1</a>
-                    <a class="subnav1" href="#">Home Sub2</a>
-                    <a class="subnav1" href="#">Home Sub3</a>
+                <div class="float-container">
+                    <a class="subnav1" href="<?= $linkpath ?>index.php">Main Page</a>
+                    <a class="subnav1" href="<?= $linkpath ?>beginner/commandline.php">The Command Line</a>
+                    <a class="subnav1" href="<?= $linkpath ?>beginner/introduction.php">Introduction</a>
+                    <a class="subnav1" href="<?= $linkpath ?>beginner/commands.php">Basic Commands</a>
                 </div>
             </div><!-- end <div class="sub-navlist"> -->
             <div class="sub-navlist">
                 <a onclick="closeSubNav(); getId(this); openSubNav()" id="intermediate-section" <?php if($page == 'intermediate'){  
                 echo 'class="active"';}?> href="#">Intermediate</a>
                 <div class="float-container">
-                    <a class="subnav2" href="intermediate.php">Intermediate Sub1</a>
-                    <a class="subnav2" href="#">Intermediate Sub2</a>
-                    <a class="subnav2" href="#">Intermediate Sub3</a>
+                    <a class="subnav2" href="<?= $linkpath ?>intermediate/intermediate.php">Intermediate Home</a>
+                    <a class="subnav2" href="<?= $linkpath ?>intermediate/modal.php">Modal Editing</a>
+                    <a class="subnav2" href="<?= $linkpath ?>intermediate/modes.php">Vi Modes</a>
                 </div>
             </div><!-- END <div class="sub-navlist> -->
             <div class="sub-navlist">
                 <a onclick="closeSubNav(); getId(this); openSubNav()" id="advanced-section" <?php if($page == 'advanced'){ 
                 echo 'class="active"';}?> href="#">Advanced</a>
                 <div class="float-container">
-                    <a class="subnav3" href="advanced.php">Advanced Sub1</a>
-                    <a class="subnav3" href="#">Advanced Sub2</a>
-                    <a class="subnav3" href="#">Advanced Sub3</a>
+                    <a class="subnav3" href="<?= $linkpath ?>advanced/advanced.php">Advanced Home</a>
+                    <a class="subnav3" href="<?= $linkpath ?>advanced/plugins.php">Plugins</a>
+                    <a class="subnav3" href="<?= $linkpath ?>advanced/review.php">Command Refresher</a>
                 </div>
             </div><!-- END <div class="sub-navlist> -->
+            <a <?php if($page == 'test'){ 
+                echo 'class="active"';}?> href="<?= $linkpath ?>test.php">Test of Skill</a>
+            <a <?php if($page == 'glossary'){ 
+                echo 'class="active"';}?> href="<?= $linkpath ?>glossary.php">Glossary</a>
+            <a <?php if($page == 'resources'){ 
+                echo 'class="active"';}?> href="<?= $linkpath ?>resources.php">Resources</a>
             <a <?php if($page == 'about'){ 
-                echo 'class="active"';}?> href="#">About</a>
+                echo 'class="active"';}?> href="<?= $linkpath ?>about.php">About</a>
     </nav>
     <nav id="myDesktopNav" class="desktopNav">
       <ul id="desktop">
