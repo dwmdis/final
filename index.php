826e3b02c9f78a2df8f1ac896393bcd974135c5c
sierra-skorupski <shs6196@rit.edu>
2017-11-17 11:15:27 -0500

    Added dummy text.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 <?php include "assets/inc/navigation.php" ?>
   <main>
+<<<<<<< Updated upstream
     <div>Home page
         <p>page = "<?php echo $page ?>"</p>
         <p>I did it!</p>
@@ -16,6 +17,26 @@
             <input type="search" placeholder="Search this site..." />
             <input type="submit" value="Search" class="btn-search"  />
         </form>
+=======
+    <div class="content" id="home"><h1>VIable</h1>
+        <!--<p>page = "<?php echo $page ?>"</p>-->
+        <p>
+	        <b>user@localhost~:$</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+	        Morbi cursus ultrices nibh a porta. 
+	        Aenean rhoncus dolor est, sit amet auctor sem ullamcorper eget. 
+	        Nulla egestas mi mauris, ut malesuada mi ultrices sit amet. 
+	        Sed posuere consectetur sapien non dignissim. 
+	        Vestibulum dictum purus et nisl varius, ac aliquet metus laoreet. 
+	        Ut porttitor semper ultricies. 
+	        Pellentesque aliquet elit sit amet rutrum finibus. 
+	        Integer ornare libero sit amet accumsan auctor. 
+	        Vestibulum placerat elit lorem, nec dictum eros molestie at. 
+	        Suspendisse quis eros metus. Sed dictum vitae risus non posuere. 
+	        Quisque ultricies arcu eu luctus dignissim. 
+	        Morbi et fringilla nunc. Duis interdum vestibulum nulla et ullamcorper.
+	        Sed ullamcorper molestie ornare.
+        </p>
+>>>>>>> Stashed changes
       </div>
   </main>
 </body>
