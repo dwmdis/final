273ca83084b3b3d93b9b86b3a69367f3c1836eb0
Dale Moore <dmoore092@gmail.com>
2017-11-17 12:55:12 -0500

    fixed some badly copy/pasted code

--- intermediate/modes.php
+++ intermediate/modes.php
@@ -7,17 +7,6 @@
 <?php include "../assets/inc/head.php" ?>
 <!-- <head> went here -->
 <body>
-<?php include "../assets/inc/navigation.php" ?>
-  <main>
-    <!DOCTYPE html>
-<html lang="en">
-<?php 
-    //Sets variables
-    $relpath= ""; $title="Vi|home"; $page="home"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
-?>
-<?php include "assets/inc/head.php" ?>
-<!-- <head> went here -->
-<body>
 <?php include "assets/inc/navigation.php" ?>
   <main>
     <div class="content" id="home"><h1>VIable</h1>
