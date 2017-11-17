f9ab839d8be61ee335be6424c52a8cc4318e10b0
Dale Moore <dmoore092@gmail.com>
2017-11-17 12:36:47 -0500

    attempt 2 at fixing links

--- assets/inc/navigation.php
+++ assets/inc/navigation.php
@@ -52,23 +52,23 @@
 	    <li><a href="#">Home</a></li>
 	    <li class="dropdown"><a href="javascript:void(0)" class="dropbtn">Beginner &#9660;</a>
 	      <div class="dropdown-content" id="beginner">
-	        <a class="subnav1" href="<?= $linkpath ?>beginner/commandline.php">The Command Line</a>
-	        <a class="subnav1" href="<?= $linkpath ?>beginner/introduction.php">Introduction</a>
-            <a class="subnav1" href="<?= $linkpath ?>beginner/commands.php">Basic Commands</a>
+	        <a href="<?= $linkpath ?>beginner/commandline.php">The Command Line</a>
+	        <a href="<?= $linkpath ?>beginner/introduction.php">Introduction</a>
+            <a href="<?= $linkpath ?>beginner/commands.php">Basic Commands</a>
 	      </div>
 	    </li>
 	    <li class="dropdown"><a href="javascript:void(0)" class="dropbtn">Intermediate &#9660;</a>
 	      <div class="dropdown-content" id="intermediate">
-	        <a class="subnav2" href="<?= $linkpath ?>intermediate/intermediate.php">Intermediate Home</a>
-            <a class="subnav2" href="<?= $linkpath ?>intermediate/modal.php">Modal Editing</a>
-            <a class="subnav2" href="<?= $linkpath ?>intermediate/modes.php">Vi Modes</a>
+	        <a href="<?= $linkpath ?>intermediate/intermediate.php">Intermediate Home</a>
+            <a href="<?= $linkpath ?>intermediate/modal.php">Modal Editing</a>
+            <a href="<?= $linkpath ?>intermediate/modes.php">Vi Modes</a>
 	      </div>
 	    </li>
 	    <li class="dropdown"><a href="javascript:void(0)" class="dropbtn">Advanced &#9660;</a>
 	      <div class="dropdown-content" id="advanced">
-	        <a class="subnav3" href="<?= $linkpath ?>advanced/advanced.php">Advanced Home</a>
-            <a class="subnav3" href="<?= $linkpath ?>advanced/plugins.php">Plugins</a>
-            <a class="subnav3" href="<?= $linkpath ?>advanced/review.php">Command Refresher</a>
+	        <a href="<?= $linkpath ?>advanced/advanced.php">Advanced Home</a>
+            <a href="<?= $linkpath ?>advanced/plugins.php">Plugins</a>
+            <a href="<?= $linkpath ?>advanced/review.php">Command Refresher</a>
 	      </div>
 	    </li>
           <li class="dropdown"><a href="<?= $linkpath ?>test.php">Test of Skill</a></li>
