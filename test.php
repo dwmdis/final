b47281dbac9f1e61b738d5b067dc05ea22fbe810
Ryan Lowrie <rml3985@g.rit.edu>
2017-11-22 15:12:50 -0500

    Added dummy text test form to test.php

--- test.php
+++ test.php
@@ -27,6 +27,109 @@
 	        Morbi et fringilla nunc. Duis interdum vestibulum nulla et ullamcorper.
 	        Sed ullamcorper molestie ornare.
         </p>
+        
+        <form method="post" action="project2quizform.html" id="quizform">
+        <p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/></span>
+            <label for="ans1">
+                <input type="radio" title="q1ans" id="ans1"/>Lorem&emsp;
+            </label>
+            <label for="ans2">
+                <input type="radio" title="q1ans" id="ans2"/>Ipsum&emsp;
+            </label>
+            <label for="ans3">
+                <input type="radio" title="q1ans" id="ans3"/>Loremipsum&emsp;
+            </label>
+            <label for="ans4">
+                <input type="radio" title="q1ans" id="ans4"/>Lor&emsp;
+            </label>
+        </p>
+        
+        <p>
+            <span>Aenean ullamcorper augue a arcu finibus, ut euismod mi porttitor. Aliquam sit:<br/></span>
+            <label for="ans1">
+                <input type="radio" title="q2ans" id="ans1"/>Lorem&emsp;
+            </label>
+            <label for="ans2">
+                <input type="radio" title="q2ans" id="ans2"/>Ipsum&emsp;
+            </label>
+            <label for="ans3">
+                <input type="radio" title="q2ans" id="ans3"/>Loremipsum&emsp;
+            </label>
+            <label for="ans4">
+                <input type="radio" title="q2ans" id="ans4"/>Lor&emsp;
+            </label>
+        </p>
+        
+        <p>
+            <span>Aenean ullamcorper augue finibus, ut euismod mi porttitor sit:<br/></span>
+            <label for="ans1">
+                <input type="radio" title="q3ans" id="ans1"/>Lorem&emsp;
+            </label>
+            <label for="ans2">
+                <input type="radio" title="q3ans" id="ans2"/>Ipsum&emsp;
+            </label>
+            <label for="ans3">
+                <input type="radio" title="q3ans" id="ans3"/>Loremipsum&emsp;
+            </label>
+            <label for="ans4">
+                <input type="radio" title="q3ans" id="ans4"/>Lor&emsp;
+            </label>
+        </p>
+        
+        <p>
+            <span>Ullamcorper augue a arcu finibus, ut euismod mi porttitor. Aliquam sit:<br/></span>
+            <label for="ans1">
+                <input type="radio" title="q4ans" id="ans1"/>Lorem&emsp;
+            </label>
+            <label for="ans2">
+                <input type="radio" title="q4ans" id="ans2"/>Ipsum&emsp;
+            </label>
+            <label for="ans3">
+                <input type="radio" title="q4ans" id="ans3"/>Loremipsum&emsp;
+            </label>
+            <label for="ans4">
+                <input type="radio" title="q4ans" id="ans4"/>Lor&emsp;
+            </label>
+        </p>
+        
+        <p>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper augue a arcu finibus, ut sit:<br/></span>
+            <label for="ans1">
+                <input type="radio" title="q5ans" id="ans1"/>Lorem&emsp;
+            </label>
+            <label for="ans2">
+                <input type="radio" title="q5ans" id="ans2"/>Ipsum&emsp;
+            </label>
+            <label for="ans3">
+                <input type="radio" title="q5ans" id="ans3"/>Loremipsum&emsp;
+            </label>
+            <label for="ans4">
+                <input type="radio" title="q5ans" id="ans4"/>Lor&emsp;
+            </label>
+        </p>
+        
+        <p>
+            <span>Consectetur adipiscing elit. Aenean ullamcorper augue a arcu finibus, ut euismod mi porttitor. Aliquam sit:<br/></span>
+            <label for="ans1">
+                <input type="radio" title="q6ans" id="ans1"/>Lorem&emsp;
+            </label>
+            <label for="ans2">
+                <input type="radio" title="q6ans" id="ans2"/>Ipsum&emsp;
+            </label>
+            <label for="ans3">
+                <input type="radio" title="q6ans" id="ans3"/>Loremipsum&emsp;
+            </label>
+            <label for="ans4">
+                <input type="radio" title="q6ans" id="ans4"/>Lor&emsp;
+            </label>
+        </p>
+        
+        <p>
+            <input type="submit" value="Submit Quiz" id="btn submit"/>
+        </p>
+    
+    </form>
     </div>
   </main>
   <?php include "assets/inc/footer.php" ?>
