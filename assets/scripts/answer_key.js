2a009721655c5322051d9c65d3da968eaadb39e3
Dale Moore <dmoore092@gmail.com>
2017-11-27 13:18:00 -0500

    added the javascript for the test

---
+++ assets/scripts/answer_key.js
@@ -0,0 +1,29 @@
+var tally = "0";
+function check(){
+    alert("check has run!");
+    if(document.getElementById("q1a2").checked){
+        tally ++;
+    }
+    if(document.getElementById("q2a1").checked){
+        tally ++;
+    }
+    if(document.getElementById("q3a1").checked){
+        tally ++;
+    }
+    if(document.getElementById("q4a4").checked){
+        tally ++;
+    }
+    if(document.getElementById("q5a2").checked){
+        tally ++;
+    }
+    if(document.getElementById("q6a3").checked){
+        tally ++;
+    }
+    if(tally >="5"){
+        alert("Passed! tally = " + tally);
+    }
+    else{
+        alert("Fail! Because tally = " + tally);
+    }
+
+}
