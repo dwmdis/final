0aa2603944f1d1b5c0795ab97fc25c0abe4e4551
Dale Moore <dmoore092@gmail.com>
2017-11-27 12:18:59 -0500

    added test of skill questions, fixed the lorem ipsum

--- test.php
+++ test.php
@@ -9,25 +9,110 @@
 <body>
 <?php include "assets/inc/navigation.php" ?>
   <main>
-    <div class="content" id="home"><h1>VIable</h1>
-        <p>page = "<?php echo $page ?>"</p>
-        <p>
-	        <b>user@localhost~:$</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-	        Morbi cursus ultrices nibh a porta. 
-	        Aenean rhoncus dolor est, sit amet auctor sem ullamcorper eget. 
-	        Nulla egestas mi mauris, ut malesuada mi ultrices sit amet. 
-	        Sed posuere consectetur sapien non dignissim. 
-	        Vestibulum dictum purus et nisl varius, ac aliquet metus laoreet. 
-	        Ut porttitor semper ultricies. 
-	        Pellentesque aliquet elit sit amet rutrum finibus. 
-	        Integer ornare libero sit amet accumsan auctor. 
-	        Vestibulum placerat elit lorem, nec dictum eros molestie at. 
-	        Suspendisse quis eros metus. Sed dictum vitae risus non posuere. 
-	        Quisque ultricies arcu eu luctus dignissim. 
-	        Morbi et fringilla nunc. Duis interdum vestibulum nulla et ullamcorper.
-	        Sed ullamcorper molestie ornare.
-        </p>
-    </div>
+      <div class="content" id="home">
+        <form method="post" action="project2quizform.html" id="quizform">
+            <p>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper augue a arcu finibus, ut euismod mi porttitor. Aliquam sit:<br/></span>
+                <label for="ans1">
+                    <input type="radio" title="q1ans" id="ans1"/>Answer1&emsp;
+                </label>
+                <label for="ans2">
+                    <input type="radio" title="q1ans" id="ans2"/>Answer2&emsp;
+                </label>
+                <label for="ans3">
+                    <input type="radio" title="q1ans" id="ans3"/>Answer3&emsp;
+                </label>
+                <label for="ans4">
+                    <input type="radio" title="q1ans" id="ans4"/>Answer4&emsp;
+                </label>
+            </p>
+        
+            <p>
+                <span>Nunc consectetur bibendum neque, eget ornare dui laoreet id. Vestibulum nec dignissim ante, eget sodales metus. Nullam nec dignissim dui:<br/></span>
+                <label for="ans1">
+                    <input type="radio" title="q2ans" id="ans1"/>Answer1&emsp;
+                </label>
+                <label for="ans2">
+                    <input type="radio" title="q2ans" id="ans2"/>Answer2&emsp;
+                </label>
+                <label for="ans3">
+                    <input type="radio" title="q2ans" id="ans3"/>Answer3&emsp;
+                </label>
+                <label for="ans4">
+                    <input type="radio" title="q2ans" id="ans4"/>Answer4&emsp;
+                </label>
+            </p>
+        
+            <p>
+                <span>In elementum eu leo nec bibendum. Morbi vulputate est ac eros ultricies dapibus. Donec dapibus pulvinar tincidunt. Vestibulum eu fermentum risus:<br/></span>
+                <label for="ans1">
+                    <input type="radio" title="q3ans" id="ans1"/>Answer1&emsp;
+                </label>
+                <label for="ans2">
+                    <input type="radio" title="q3ans" id="ans2"/>Answer2&emsp;
+                </label>
+                <label for="ans3">
+                    <input type="radio" title="q3ans" id="ans3"/>Answer3&emsp;
+                </label>
+                <label for="ans4">
+                    <input type="radio" title="q3ans" id="ans4"/>Answer4&emsp;
+                </label>
+            </p>
+        
+            <p>
+                <span>Pellentesque pretium enim nibh. Suspendisse nec sodales arcu, quis sagittis justo. Vestibulum sollicitudin sed orci a euismod. Duis non ex ex:<br/></span>
+                <label for="ans1">
+                    <input type="radio" title="q4ans" id="ans1"/>Answer1&emsp;
+                </label>
+                <label for="ans2">
+                    <input type="radio" title="q4ans" id="ans2"/>Answer2&emsp;
+                </label>
+                <label for="ans3">
+                    <input type="radio" title="q4ans" id="ans3"/>Answer3&emsp;
+                </label>
+                <label for="ans4">
+                    <input type="radio" title="q4ans" id="ans4"/>Answer4&emsp;
+                </label>
+            </p>
+        
+            <p>
+                <span>Donec sed posuere justo. In semper consequat nisl, quis viverra diam fringilla ut. Orci varius natoque penatibus:<br/></span>
+                <label for="ans1">
+                    <input type="radio" title="q5ans" id="ans1"/>Answer1&emsp;
+                </label>
+                <label for="ans2">
+                    <input type="radio" title="q5ans" id="ans2"/>Answer2&emsp;
+                </label>
+                <label for="ans3">
+                    <input type="radio" title="q5ans" id="ans3"/>Answer3&emsp;
+                </label>
+                <label for="ans4">
+                    <input type="radio" title="q5ans" id="ans4"/>Answer4&emsp;
+                </label>
+            </p>
+        
+            <p>
+                <span>Duis cursus feugiat magna, sed porttitor tellus porta dignissim. Nunc urna libero, pellentesque a volutpat non, rutrum id mauris. :<br/></span>
+                <label for="ans1">
+                    <input type="radio" title="q6ans" id="ans1"/>Answer1&emsp;
+                </label>
+                <label for="ans2">
+                    <input type="radio" title="q6ans" id="ans2"/>Answer2&emsp;
+                </label>
+                <label for="ans3">
+                    <input type="radio" title="q6ans" id="ans3"/>Answer3&emsp;
+                </label>
+                <label for="ans4">
+                    <input type="radio" title="q6ans" id="ans4"/>Answer4&emsp;
+                </label>
+            </p>
+        
+            <p>
+                <input type="submit" value="Submit Quiz" id="btn submit"/>
+            </p>
+    
+        </form>
+      </div>
   </main>
   <?php include "assets/inc/footer.php" ?>
 </body>
