9178798f2fdcf7aa526e0f8b7947050e7116689b
Klahr Clark <kec9020@rit.edu>
2017-11-28 00:13:29 -0500

    Adding content
    Added content to the command-line section.

--- beginner/commandline.php
+++ beginner/commandline.php
@@ -12,20 +12,14 @@
     <div class="content" id="home"><h1>VIable</h1>
         <p>page = "<?php echo $page ?>"</p>
         <p>
-	        <b>user@localhost~:$</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-	        Morbi cursus ultrices nibh a porta. 
-	        Aenean rhoncus dolor est, sit amet auctor sem ullamcorper eget. 
-	        Nulla egestas mi mauris, ut malesuada mi ultrices sit amet. 
-	        Sed posuere consectetur sapien non dignissim. 
-	        Vestibulum dictum purus et nisl varius, ac aliquet metus laoreet. 
-	        Ut porttitor semper ultricies. 
-	        Pellentesque aliquet elit sit amet rutrum finibus. 
-	        Integer ornare libero sit amet accumsan auctor. 
-	        Vestibulum placerat elit lorem, nec dictum eros molestie at. 
-	        Suspendisse quis eros metus. Sed dictum vitae risus non posuere. 
-	        Quisque ultricies arcu eu luctus dignissim. 
-	        Morbi et fringilla nunc. Duis interdum vestibulum nulla et ullamcorper.
-	        Sed ullamcorper molestie ornare.
+	        <b>user@localhost~:$</b>
+	        The command-line is a useful tool for you to use as you become more experienced in programming. To use, all you have to do is enter in a valid command with any parameters. Commands must have correct spelling and syntax in order to work. We are focusing on how to use vi on the command-line to edit code. But first you need to learn how to use the command-line. <br/>
+
+	        There are many command-line interpreters to use. Two of the most popular on windows devices are the Command Prompt and Windows PowerShell. The command-line interface is the Terminal. <br/>
+
+	        Once you have the command-line open, you can start using VI! First, you need to navigate to the directory the file you wish to edit is. In orderto see where you currently are use pwd. In order to enter a directory, use: cd <i>directory name</i>. Finally to pull up the file in Vi, use: vi <i>filename</i>. You should now be all set to start using vi, navigate to the commands tab to learn more about how to edit with vi.
+
+
         </p>
     </div>
   </main>
