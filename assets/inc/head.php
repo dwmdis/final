5024bf1825c99ff4a1721f6d7337ef8d46a10ff6
sierra-skorupski <shs6196@rit.edu>
2017-12-01 11:15:42 -0500

    Added active navigation class to global navbar.

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -4,5 +4,6 @@
     <title><?php echo $title; ?></title>
     <link type="text/css" href="<?= $relpath ?>assets/css/main.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="assets/images/logo-final-icon.png">
+    <script src="<?php echo $relpath ?>assets/scripts/activeNav.js"></script>
     <script src="<?php echo $relpath ?>assets/scripts/mobileNav.js"></script>
 </head>
