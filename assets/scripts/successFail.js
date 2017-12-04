3e42cd79fe19d4cba5533ae2be105fef7b6afb31
Dale Moore <dmoore092@gmail.com>
2017-12-04 13:37:30 -0500

    renamed a couple of scripts

---
+++ assets/scripts/successFail.js
@@ -0,0 +1,39 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    if(success){
+        modal.style.display = "none";
+        window.location = "certificate.html";
+    }
+    else{
+        modal.style.display = "none";
+        event.preventDefault();
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        if(success){
+        modal.style.display = "none";
+        window.location = "certificate.html";
+        }
+        else{
+            modal.style.display = "none";
+            event.preventDefault();
+        }
+    }
+}
