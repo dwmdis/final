925828aaae1b09103143681349ce7feed8fcb50a
Dale Moore <dmoore092@gmail.com>
2017-12-04 13:45:35 -0500

    styled certificate.php

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <title><?php echo $title; ?></title>
     <link type="text/css" href="<?= $relpath ?>assets/css/main.css" rel="stylesheet">
+    <link type="text/css" href="<?= $relpath ?>assets/css/certificate.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="assets/images/logo-final-icon.png">
     <script src="<?php echo $relpath ?>assets/scripts/activeNav.js"></script>
     <script src="<?php echo $relpath ?>assets/scripts/mobileNav.js"></script>
---
+++ certificate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+    //Sets variables
+    $relpath= ""; $title="Vi|home"; $page="home"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
+?>
+<?php include "assets/inc/head.php" ?>
+<?php include "assets/css/certificate.css" ?>
+<body>
+<?php include "assets/inc/navigation.php" ?>
+  <main>
+    <div class="content" id="home">
+    	<img id="logo" src="assets/images/logo-final.png">
+    	<div class="certificate">
+		<h1>Vi Certificate</h1>
+		<img id ="cert" src="assets/images/beginner_certificate.png">
+		<br>
+		<a href="">Download</a>
+	</div>
+    </div>
+  </main>
+  <?php include "assets/inc/footer.php" ?>
+</body>
+</html>
