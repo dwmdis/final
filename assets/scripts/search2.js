44be3a4cdaa385e716ecb335b099495d64185680
Dale Moore <dmoore092@gmail.com>
2017-12-06 15:14:42 -0500

    working on search logic

--- assets/scripts/search2.js
+++ assets/scripts/search2.js
@@ -1,5 +1,6 @@
 var smodal = document.getElementById("searchModal");
 var span = document.getElementsByClassName("close")[0];
+var found = false;
 var tag_map = {
     home: "index.php",
     introduction: "index.php",
@@ -30,11 +31,12 @@ function search(){
         if(i === search_string){
             console.log("tag_map[i] = " + tag_map[i]);
             window.location = "http://serenity.ist.rit.edu/~iste240t04/240/final/" + tag_map[i];
+            found = true;
         }
-        else{
-            document.getElementById("searchModal").style.display = "block";
-            document.getElementById("searchModal").innerHTML = "<p id='modal-insert'>Sorry, we couldn't find that page!<br/>Try searching for a page title.</p>";
-        }
+    }
+    if(!found){
+        document.getElementById("searchModal").style.display = "block";
+        document.getElementById("searchModal").innerHTML = "<p id='modal-insert'>Sorry, we couldn't find that page!<br/>Try searching for a page title.</p>";
     }
 }
 // When the user clicks anywhere outside of the modal, close it
