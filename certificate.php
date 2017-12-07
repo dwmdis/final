dd634f5d82aee35c7d3299712a22686eccbe8872
Dale Moore <dmoore092@gmail.com>
2017-12-07 09:06:53 -0500

    fixing bugs from validator

--- certificate.php
+++ certificate.php
@@ -10,10 +10,10 @@
 <?php include "assets/inc/navigation.php" ?>
   <main>
     <div class="content" id="home">
-    	<img id="logo" src="assets/images/logo-final.png">
+    	<img id="logo" src="assets/images/logo-final.png" alt ="logo">
     	<div class="certificate">
 		<h1>Vi Certificate</h1>
-		<img id ="cert" src="assets/images/beginner_certificate.png">
+		<img id ="cert" src="assets/images/beginner_certificate.png" alt = "image of certificate">
 		<br>
 		<a href="">Download</a>
 	</div>
