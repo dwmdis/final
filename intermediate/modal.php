13359c20eff14b29d99dcb087c646109e7c35293
Ryan Lowrie <rml3985@g.rit.edu>
2017-12-07 08:18:18 -0500

    Renaming of modal page to intermediate commands

--- intermediate/modal.php
+++ intermediate/modal.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <?php 
     //Sets variables
-    $relpath= "../"; $title="Vi|Modal Editing"; $page="modal"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
+    $relpath= "../"; $title="Vi|Intermediate Commands"; $page="intermediate commands"; $imgpath="http://serenity.ist.rit.edu/~iste240t04/240/final/assets/media/images/";
 ?>
 <?php include "../assets/inc/head.php" ?>
 <!-- <head> went here -->
