bf98ec78104d3fbdec1b6ffa546be4f697971b21
Klahr Clark <kec9020@rit.edu>
2017-12-07 08:07:47 -0500

    about page

--- about.php
+++ about.php
@@ -12,20 +12,16 @@
     <div class="content" id="home"><h1>VIable</h1>
         <p><b>user@localhost~:$</b> $page = <?php echo $page ?>.php</p>
         <p>
-	        <b>user@localhost~:$</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-	        Morbi cursus ultrices nibh a porta. 
-	        Aenean rhoncus dolor est, sit amet auctor sem ullamcorper eget. 
-	        Nulla egestas mi mauris, ut malesuada mi ultrices sit amet. 
-	        Sed posuere consectetur sapien non dignissim. 
-	        Vestibulum dictum purus et nisl varius, ac aliquet metus laoreet. 
-	        Ut porttitor semper ultricies. 
-	        Pellentesque aliquet elit sit amet rutrum finibus. 
-	        Integer ornare libero sit amet accumsan auctor. 
-	        Vestibulum placerat elit lorem, nec dictum eros molestie at. 
-	        Suspendisse quis eros metus. Sed dictum vitae risus non posuere. 
-	        Quisque ultricies arcu eu luctus dignissim. 
-	        Morbi et fringilla nunc. Duis interdum vestibulum nulla et ullamcorper.
-	        Sed ullamcorper molestie ornare.
+	        <b>user@localhost~:$</b> About our project.
+	        <p>
+		        	This tutorial site was created for a group project in ISTE-240. This group was made up of:<br/>
+					Dale Moore – Project Leader/Programmer<br/>
+					Sierra Skorupski – Information Design/Programmer<br/>
+					Ryan Lowrie – Interaction Design<br/>
+					Klahr Clark – Information Design/Content<br/>
+					Nakeia Barlow – Artist/Content <br/>
+
+	        </p>
         </p>
     </div>
   </main>
--- beginner/introduction.php
+++ beginner/introduction.php
@@ -15,24 +15,24 @@
 	        <b>user@localhost~:$</b>
 	       <p>
             Vi is the default text editor in Unix, and Linux. It is fount in almost every system of the type. Since it is almost always there, it is good to have some basic knowledge of vi if you ever must use it, even if it is not your usual editor. Vi is not intuitive and can be difficult to get the hang of, hopefully after using this site you will be ready to start using the editor. Remember practice makes perfect, it will become easier to use after time.
-         </p><br/>
+         </p>
 
          <p>
            Commands are very powerful in vi. If you are not sure if what you are putting in is correct, be sure to double check or you could make major changes to your program. There are about 150 commands in vi, but it is not necessary to learn all, or even most them, to be efficient. In this site we will only be covering the essential commands to know and some more advanced that can be helpful.
 
-         </p> <br/>
+         </p> 
 
          <p>
            Vi is a text-editor originally created to be used with the Unix operating system. Vi was developed by Bill Joy in 1976. It was originally created to be the full screen visual mode of a different editor, ex. Joy developed ex as a improved line editor to the original Unix line editor ex. Vi gets its name since it is the visual interface of ed, and allowed a user to view text on the full screen, rather than line by line. Inspiration for VI from the Bravo editor, and Joy admits that most of the “stuff” that makes up vi was stolen from other editors. 
-         </p> <br/>
+         </p> 
 
          <p>
            Joy created the first BSD, Berkley Software Distribution, Unix in 1978. He included ex in the distribution, thus exposing his audience to vi for the first time. When he found that most users were only using vi, he made it a hard link to ex in the next release. Nowadays, vi is known for how small it is, with some versions being less than 100KB. However, by the third version of vi, it was too large to fit in the memory on many computers. In 1979 Bill Joy stepped down as the lead developer and Mark Holton took the role. He added support for arrow keys and improved performance.
-         </p><br/>
+         </p>
 
          <p>
            Today, vi is still widely used by users. In 1991, a poll found that a majority of users preferred vi. There have been many text editors made to be similar or build of vi, one of the most popular examples of this is vim. 
-         </p><br/>      
+         </p>     
           	        
 
         </p>
